0649d0dbdf8eaa907bee4e156de12337170ba102
1Gokul <viswanath1gokul@gmail.com>
2021-09-03 18:26:57 +0530

    Few changes to input

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 
 import Layout, { Container } from "../src/components/Layout"
-import { Flex, Heading, Input } from "@chakra-ui/react"
+import { Button, Flex, Heading, Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
+import { Search2Icon } from "@chakra-ui/icons"
 
 export default function Home() {
   return (
     <Layout>
       <Container>
         <Heading>Get information on available PC components and peripherals in India</Heading>
-        <Flex marginTop={10}>
-          <Input size="lg" placeholder="Search for a component..."/>
+        <Flex marginY={10}>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              height="100%"
+              children={<Search2Icon color="gray.400"/>}  //eslint-disable-line
+            />
+            <Input size="lg" placeholder="Search for a component..." variant="flushed"/>
+          </InputGroup>
         </Flex>
+        <Button alignSelf="center" fontSize="xl" paddingX={12} paddingY={8} >Search</Button>
       </Container>
     </Layout>
   )
