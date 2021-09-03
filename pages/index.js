e4273121e0b0aa5f6f05f5275150220073bd0aa6
1Gokul <viswanath1gokul@gmail.com>
2021-09-03 12:27:50 +0530

    Added header

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,8 @@ import { Flex, Heading } from "@chakra-ui/react"
 export default function Home() {
   return (
     <Layout>
-      <Flex direction="column" justifyContent="center">
-
-        <Heading alignSelf="center" fontSize="100px">PCPartFinder</Heading>
+      <Flex direction="column" flexGrow={1} alignItems="center" justifyContent="center">
+        <Heading fontSize="100px">PCPartFinder</Heading>
       </Flex>
     </Layout>
   )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,15 @@
-import { Flex } from "@chakra-ui/layout"
+import { Flex, Heading } from "@chakra-ui/react"
 
 const Layout = props => (
-  <Flex direction="column" margin="0 auto">
+  <Flex direction="column" margin="0 auto" height="100vh">
+    <Header />
     {props.children}
+  </Flex>
+)
+
+const Header = () => (
+  <Flex justifyContent="space-between" padding={{ base: 5, md: 6 }}>
+    <Heading fontSize="4xl">PCPartFinder</Heading>
   </Flex>
 )
 
