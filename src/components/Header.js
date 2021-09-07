d22863eed271aa3f88d9cb3cfa22777372236098
1Gokul <viswanath1gokul@gmail.com>
2021-09-07 19:16:26 +0530

    Fixed mobile toggler

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,6 +46,8 @@ const Header = () => {
       <MobileNavMenu
         navLinks={navLinks}
         expanded={expanded}
+        colorMode={colorMode}
+        toggleColorMode={toggleColorMode}
       />
     </Flex>
   )
