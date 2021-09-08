683ad09bb821f1f07faa843595a5e7e0b296cc45
1Gokul <viswanath1gokul@gmail.com>
2021-09-09 00:27:11 +0530

    Added Set Alert page

---
+++ pages/set-alert.js
@@ -0,0 +1,15 @@
+import { Heading } from "@chakra-ui/layout"
+
+import Layout, { Container } from "../src/components/Layout"
+
+const SetAlert = () => {
+  return (
+    <Layout title="Set Alert" page="/set-alert">
+      <Container>
+        <Heading size="2xl">Set Alert (WIP)</Heading>
+      </Container>
+    </Layout>
+  )
+}
+
+export default SetAlert
