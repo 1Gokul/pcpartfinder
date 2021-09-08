a551737eb531ecaf83177b0f1b069016a0b655cc
1Gokul <viswanath1gokul@gmail.com>
2021-09-08 23:56:45 +0530

    Extracted tables into their own component

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,16 @@ import {
   Flex,
   Heading,
   Icon,
-  Link,
   Text,
-  Table,
-  Thead,
-  Tbody,
-  TableCaption,
-  Tr,
-  Th,
-  Td,
   Progress,
   useStyleConfig,
 } from "@chakra-ui/react"
 import axios from "axios"
-import { IoCart } from "react-icons/io5"
 import { GoPrimitiveDot, GoChevronUp, GoChevronDown } from "react-icons/go"
 
 import Layout, { Container } from "../src/components/Layout"
 import Form from "../src/components/Form"
+import { ResultTable, StoreTable } from "../src/components/Tables"
 
 const Home = () => {
   const [results, setResults] = useState ({})
@@ -121,59 +113,5 @@ const ResultViewer = ({ search_results }) => {
     </Flex>
   )
 }
-
-const StoreTable = ({ item }) => (
-  <Flex direction="column" marginBottom={16}>
-    <Heading size="2xl" marginBottom={4}>{item.store}</Heading>
-
-    {item.results.length
-      ? <ResultTable items={item.results} />
-      : <Text size="xl">
-          No matching products found.
-      </Text>}
-  </Flex>
-)
-
-const ResultTable = ({ items }) => (
-  <Flex overflowX="auto">
-    <Table
-      size="lg"
-      variant="striped"
-      colorScheme="cyan"
-      border="2px"
-      borderColor="cyan.600"
-    >
-      <TableCaption display={{ md: "none" }} textAlign="left" placement="top">
-        ← Swipe left to see other columns
-      </TableCaption>
-      <Thead>
-        <Tr>
-          <Th>Product</Th>
-          <Th>Price</Th>
-          <Th>Link</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {items.map (result => (
-          <Tr key={result.name}>
-            <Td>
-              <Text noOfLines={2}>
-                {result.name}
-              </Text>
-            </Td>
-            <Td>
-              <strong>{result.price === 0 ? "Call Store": `₹${result.price}`}</strong>
-            </Td>
-            <Td>
-              <Link variant="storeLink" target="_blank" href={result.link}>
-                <Icon as={IoCart} fontSize="2xl" />
-              </Link>
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
-  </Flex>
-)
 
 export default Home
---
+++ src/components/Tables.js
@@ -0,0 +1,71 @@
+import {
+  Flex,
+  Heading,
+  Icon,
+  Link,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  TableCaption,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react"
+import { IoCart } from "react-icons/io5"
+
+export const StoreTable = ({ item }) => (
+  <Flex direction="column" marginBottom={16}>
+    <Heading size="2xl" marginBottom={4}>{item.store}</Heading>
+
+    {item.results.length
+      ? <ResultTable items={item.results} />
+      : <Text size="xl">
+          No matching products found.
+      </Text>}
+  </Flex>
+)
+
+export const ResultTable = ({ items }) => (
+  <Flex overflowX="auto">
+    <Table
+      size="lg"
+      variant="striped"
+      colorScheme="cyan"
+      border="2px"
+      borderColor="cyan.600"
+    >
+      <TableCaption display={{ md: "none" }} textAlign="left" placement="top">
+        ← Swipe left to see other columns
+      </TableCaption>
+      <Thead>
+        <Tr>
+          <Th>Product</Th>
+          <Th>Price</Th>
+          <Th>Link</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {items.map (result => (
+          <Tr key={result.name}>
+            <Td>
+              <Text noOfLines={2}>
+                {result.name}
+              </Text>
+            </Td>
+            <Td>
+              <strong>
+                {result.price === 0 ? "Call Store" : `₹${result.price}`}
+              </strong>
+            </Td>
+            <Td>
+              <Link variant="storeLink" target="_blank" href={result.link}>
+                <Icon as={IoCart} fontSize="2xl" />
+              </Link>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  </Flex>
+)
