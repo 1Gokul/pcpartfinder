312548787f82a6b8c431cd6e5cb6b38a68cc55e5
1Gokul <viswanath1gokul@gmail.com>
2021-09-10 18:59:00 +0530

    Modified acessibility outlines

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import { ChakraProvider } from "@chakra-ui/react"
+import "focus-visible/dist/focus-visible"
+import { Global, css } from "@emotion/react"
+
 import customTheme from "../styles/theme"
+
+// https://medium.com/@keeganfamouss/accessibility-on-demand-with-chakra-ui-and-focus-visible-19413b1bc6f9
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */  .js-focus-visible :focus:not([data-focus-visible-added]) {
+     outline: none;
+     box-shadow: none;
+   }
+`
 
 function App({ Component, pageProps }) {
   return(
     <ChakraProvider theme={customTheme}>
+      <Global styles={GlobalStyles} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
