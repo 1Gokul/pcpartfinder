128f19824a4dcb3b894b8ab45ef680b691c388f1
1Gokul <viswanath1gokul@gmail.com>
2021-09-11 18:17:50 +0530

    Made table responsive

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -11,6 +11,7 @@ import {
   Tr,
   Th,
   Td,
+  useMediaQuery
 } from "@chakra-ui/react"
 import { RiExternalLinkLine } from "react-icons/ri"
 
@@ -26,46 +27,49 @@ export const StoreTable = ({ item }) => (
   </Flex>
 )
 
-export const ResultTable = ({ items }) => (
-  <Flex overflowX="auto">
-    <Table
-      size="lg"
-      variant="striped"
-      colorScheme="cyan"
-      border="2px"
-      borderColor="cyan.600"
-    >
-      <TableCaption display={{ md: "none" }} textAlign="left" placement="top">
+export const ResultTable = ({ items }) => {
+  const [largerThanMD] = useMediaQuery("(min-width: 784px)")
+  return(
+    <Flex overflowX="auto">
+      <Table
+        size={largerThanMD ? "lg" : "sm"}
+        variant="striped"
+        colorScheme="cyan"
+        border="2px"
+        borderColor="cyan.600"
+      >
+        <TableCaption display={{ md: "none" }} textAlign="left" placement="top">
         ← Swipe left to see other columns
-      </TableCaption>
-      <Thead>
-        <Tr>
-          <Th>Product</Th>
-          <Th>Price</Th>
-          <Th>Link</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {items.map (result => (
-          <Tr key={result.name}>
-            <Td>
-              <Text noOfLines={2}>
-                {result.name}
-              </Text>
-            </Td>
-            <Td>
-              <strong>
-                {result.price === 0 ? "Call Store" : `₹${result.price}`}
-              </strong>
-            </Td>
-            <Td>
-              <Link target="_blank" href={result.link}>
-                <Icon as={RiExternalLinkLine} fontSize="2xl" />
-              </Link>
-            </Td>
+        </TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Product</Th>
+            <Th>Price</Th>
+            <Th>Link</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
-  </Flex>
-)
+        </Thead>
+        <Tbody>
+          {items.map (result => (
+            <Tr key={result.name}>
+              <Td>
+                <Text noOfLines={4}>
+                  {result.name}
+                </Text>
+              </Td>
+              <Td>
+                <strong>
+                  {result.price === 0 ? "Call Store" : `₹${result.price}`}
+                </strong>
+              </Td>
+              <Td>
+                <Link target="_blank" href={result.link}>
+                  <Icon as={RiExternalLinkLine} fontSize="2xl" />
+                </Link>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Flex>
+  )
+}
