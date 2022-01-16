cdb8458da5b6f2ba74d494c0edabf334d0994389
1Gokul <viswanath1gokul@gmail.com>
2022-01-16 22:48:26 +0530

    Remove duplicate table call

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -15,59 +15,54 @@ import {
 
 type tableProps = {
   items: { name: string; url: string; price: number; store: string }[];
-  [otherProps: string]: unknown;
 };
 
-const Table: React.FC<tableProps> = (props) => {
-  const { items, ...otherProps } = props;
-
-  return (
-    <Flex
-      css={css`
-        overflow-x: "auto";
-      `}
-    >
-      {items.length ? (
-        <StyledTable {...otherProps}>
-          <Thead>
-            <Tr>
-              <Th>Product</Th>
-              <Th>Price</Th>
-              <Th>Link</Th>
+const Table = ({ items, ...otherProps }: tableProps) => (
+  <Flex
+    css={css`
+      overflow-x: "auto";
+    `}
+  >
+    {items.length ? (
+      <StyledTable {...otherProps}>
+        <Thead>
+          <Tr>
+            <Th>Product</Th>
+            <Th>Price</Th>
+            <Th>Link</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {items.map((result) => (
+            <Tr key={`${result.name}:${result.url}`}>
+              <Td>
+                <a href={result.url} target="_blank" rel="noreferrer">
+                  {result.name}
+                </a>
+              </Td>
+              <Td>
+                <strong>
+                  {result.price === 0
+                    ? "Call Store"
+                    : `₹${result.price.toLocaleString("en-IN")}`}
+                </strong>
+              </Td>
+              <Td>
+                <a href={result.url} target="_blank" rel="noreferrer">
+                  <Icon margin="0.25rem 0">
+                    <BiLinkExternal size={20} />
+                  </Icon>
+                </a>
+              </Td>
             </Tr>
-          </Thead>
-          <Tbody>
-            {items.map((result) => (
-              <Tr key={`${result.name}:${result.url}`}>
-                <Td>
-                  <a href={result.url} target="_blank" rel="noreferrer">
-                    {result.name}
-                  </a>
-                </Td>
-                <Td>
-                  <strong>
-                    {result.price === 0
-                      ? "Call Store"
-                      : `₹${result.price.toLocaleString("en-IN")}`}
-                  </strong>
-                </Td>
-                <Td>
-                  <a href={result.url} target="_blank" rel="noreferrer">
-                    <Icon margin="0.25rem 0">
-                      <BiLinkExternal size={20} />
-                    </Icon>
-                  </a>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </StyledTable>
-      ) : (
-        <TextBox size="xl">Sorry, no matching products were found.</TextBox>
-      )}
-    </Flex>
-  );
-};
+          ))}
+        </Tbody>
+      </StyledTable>
+    ) : (
+      <TextBox size="xl">Sorry, no matching products were found.</TextBox>
+    )}
+  </Flex>
+);
 
 export default Table;
 
@@ -107,20 +102,7 @@ export const TableWithHeading: React.FC<tableWithHeadingProps> = (props) => {
             {items.length} {items.length !== 1 ? "MATCHES" : "ITEM"}
           </TextBox>
         </Flex>
-        {/* Have to render two separate tables, one for large screens and one for small screens
-      as useMediaQuery does not work with SSR. */}
-        <Table
-          items={items}
-          size="lg"
-          display={{ base: "none", md: "table" }}
-          otherProps={otherProps}
-        />
-        <Table
-          items={items}
-          size="sm"
-          display={{ base: "table", md: "none" }}
-          otherProps={otherProps}
-        />
+        <Table items={items} {...otherProps} />
       </Flex>
     );
   } else return null;
