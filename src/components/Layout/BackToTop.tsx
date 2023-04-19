b72a184744bbb7ddbb5d95bbb4a733602f766b6b
1gokul <viswanath1gokul@gmail.com>
2023-04-20 01:25:26 +0530

    feat: Fixed backtotop button styles

--- src/components/Layout/BackToTop.tsx
+++ src/components/Layout/BackToTop.tsx
@@ -1,19 +1,19 @@
 import { IconButton } from "@chakra-ui/react";
-import { VscArrowUp } from "react-icons/vsc";
+import { RxArrowUp } from "react-icons/rx";
 import { animateScroll } from "react-scroll";
 
 const BackToTop = ({ visible }: { visible: boolean }) => (
   <IconButton
     display={visible ? "flex" : "none"}
     colorScheme="green"
-    icon={<VscArrowUp />}
+    icon={<RxArrowUp />}
     position="fixed"
+    color="green.1100"
     right={{ base: "50px", md: "70px" }}
     bottom={{ base: "50px", md: "100px" }}
-    _hover={{ bgColor: "green.600", color: "gray.100" }}
+    _hover={{ bgColor: "green.600" }}
     _active={{
-      bgColor: "green.700",
-      color: "gray.100"
+      bgColor: "green.700"
     }}
     aria-label="Click on this button to scroll to the top of the page."
     onClick={() => animateScroll.scrollToTop()}
