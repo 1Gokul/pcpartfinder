bae0da4596e1d75b58b652a643e57a2dae3619d8
1gokul <viswanath1gokul@gmail.com>
2023-10-23 01:14:58 +0530

    build: set up details skeleton

--- src/components/Table/TableRowDetails.tsx
+++ src/components/Table/TableRowDetails.tsx
@@ -1,5 +1,6 @@
-import { Box, Collapse, Flex } from "@chakra-ui/react";
+import { Box, Collapse, Flex, Skeleton, SkeletonText } from "@chakra-ui/react";
 
+import { useItemDetailQuery } from "../../utils/hooks/queries/useItemDetailQuery";
 import { Chart } from "../chart/Chart";
 
 const data1 = [
@@ -9,13 +10,58 @@ const data1 = [
   { date: "2023-03-18", price: 37240 }
 ];
 
-export const TableRowDetails = ({ isOpen }: { isOpen: boolean }) => {
+const SkeletonLine = () => (
+  <SkeletonText
+    startColor="green.400"
+    endColor="green.600"
+    noOfLines={1}
+    skeletonHeight="2"
+  />
+);
+export const TableRowDetails = ({
+  isOpen,
+  id
+}: {
+  isOpen: boolean;
+  id: string;
+}) => {
+  const { data, isLoading, isSuccess } = useItemDetailQuery(
+    id,
+    isOpen
+  );
+
   return (
     <Collapse in={isOpen} animateOpacity unmountOnExit>
-      <Flex backgroundColor="inherit">
-        <Box pb={3} pl={3}>
-          <Chart data={{ testData: data1 }} />
-        </Box>
+      <Flex py={3} px={12} gap={3} justifyContent="center">
+        {isLoading ? (
+          <>
+            <Skeleton
+              startColor="green.400"
+              endColor="green.600"
+              height="200"
+              width={400}
+              flex={3}
+            />
+            <Flex
+              direction="column"
+              gap={4}
+              flex={2}
+              px={8}
+              justifyContent="center"
+            >
+              <SkeletonLine />
+              <SkeletonLine />
+              <SkeletonLine />
+              <SkeletonLine />
+            </Flex>
+          </>
+        ) : (
+          <Flex backgroundColor="inherit">
+            <Box pb={3} pl={3}>
+              <Chart data={{ testData: data1 }} />
+            </Box>
+          </Flex>
+        )}
       </Flex>
     </Collapse>
   );
--- src/utils/hooks/queries/useItemDetailQuery.ts
+++ src/utils/hooks/queries/useItemDetailQuery.ts
@@ -3,5 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { ItemDetail } from "../../../types/itemDetail";
 import { client } from "../../common/client";
 
-export const useItemDetailQuery = (id: string) =>
-  useQuery(["itemDetail", id], () => client<ItemDetail>("/item/" + id));
+export const useItemDetailQuery = (id: string, enabled: boolean) =>
+  useQuery(["itemDetail", id], () => client<ItemDetail>("/item/" + id), {
+    enabled
+  });
